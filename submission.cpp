b1a2686a01cb76a6f6ce5e6c13cb22de6aab4c36
Yaroslav Pugach <pugach.yaroslav2@gmail.com>
2020-12-16 17:15:09 +0200

    Create the Haar cascade based detector

--- submission.cpp
+++ submission.cpp
@@ -6,6 +6,7 @@
 #include <opencv2/imgproc.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
+#include <opencv2/objdetect.hpp>
 
 class ImageFilter
 {
@@ -18,6 +19,58 @@ public:
 };	// ImageFilter
 
 
+class Detector
+{
+public:
+	virtual ~Detector() = default;
+
+	virtual void detect(const cv::Mat &image, std::vector<cv::Mat>& rois) = 0;
+
+	virtual void detect(const cv::Mat &image, std::vector<cv::Rect>& rects) = 0;
+};	// Detector
+
+class HaarDetector : public Detector
+{
+public:
+
+	HaarDetector(const cv::String &fileName, double scaleFactor = 1.1, double minNeighbors = 3, 
+		int flags = 0, cv::Size minSize = cv::Size(), cv::Size maxSize = cv::Size());
+
+	// TODO: define getters and setters
+
+	virtual void detect(const cv::Mat &image, std::vector<cv::Mat>& rois) override;
+
+	virtual void detect(const cv::Mat &image, std::vector<cv::Rect>& rects) override;
+
+private:
+	cv::CascadeClassifier cascadeClassifier;
+	double scaleFactor, minNeighbors;
+	int flags;
+	cv::Size minSize, maxSize;
+};	// HaarFaceDetector
+
+HaarDetector::HaarDetector(const cv::String &fileName, double scaleFactor, double minNeighbors, int flags, cv::Size minSize, cv::Size maxSize)
+	: cascadeClassifier(fileName)
+	, scaleFactor(scaleFactor)
+	, minNeighbors(minNeighbors)
+	, flags(flags)
+	, minSize(std::move(minSize))
+	, maxSize(std::move(maxSize))
+{
+	CV_Assert(!this->cascadeClassifier.empty());
+}
+
+void HaarDetector::detect(const cv::Mat &image, std::vector<cv::Mat>& rois)
+{
+	// TODO
+}
+
+void HaarDetector::detect(const cv::Mat &image, std::vector<cv::Rect>& rects)
+{
+	// TODO
+}
+
+
 class SunglassesFilter : public ImageFilter
 {
 public:
@@ -28,8 +81,32 @@ public:
 	virtual cv::Mat apply(const cv::Mat& image) override;
 
 private:
-	std::string fileName;
+	HaarDetector faceDetector, eyeDetector;
+	//cv::CascadeClassifier faceClassifier, eyeClassifier;
+	cv::Mat sunglasses;
 };	// SunglassesFilter
+
+SunglassesFilter::SunglassesFilter(const std::string& fileName)
+	: faceDetector("./haarcascades/haarcascade_frontalface_default.xml")
+	, eyeDetector("./haarcascades/haarcascade_eye.xml")
+	, sunglasses(cv::imread(fileName, cv::IMREAD_UNCHANGED))
+{
+	//CV_Assert(!this->faceClassifier.empty());
+	//CV_Assert(!this->eyeClassifier.empty());
+	CV_Assert(!this->sunglasses.empty());
+	CV_Assert(this->sunglasses.channels() == 4);
+}
+
+void SunglassesFilter::applyInPlace(cv::Mat& image)
+{
+}	// applyInPlace
+
+cv::Mat SunglassesFilter::apply(const cv::Mat& image)
+{
+	cv::Mat imageCopy = image.clone();
+	SunglassesFilter::applyInPlace(imageCopy);
+	return imageCopy;
+}	// apply
 
 
 int main(int argc, char* argv[])
@@ -48,6 +125,8 @@ int main(int argc, char* argv[])
 		cv::Mat imOut = filter.apply(imInput);
 		cv::imshow("output", imOut);
 		cv::waitKey();
+
+		cv::destroyAllWindows();
 
 		return 0;
 	}
