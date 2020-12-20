abc66ffecb4f39576f02f1f40354bf078b4168e2
Yaroslav Pugach <pugach.yaroslav2@gmail.com>
2020-12-20 10:59:46 +0200

    Handle command line arguments

--- submission.cpp
+++ submission.cpp
@@ -850,21 +850,68 @@ std::unique_ptr<MediaSink> MediaFactory::createWriter(const std::string& output,
 }	// createWriter
 
 
+void printUsage()
+{
+	std::cout << "Usage: glassify.exe [-h]"
+				 " [--sunglasses=<sunglasses image file>]" 
+				 " [--reflection=<reflection image file>]"
+				 " [--input=<input image, video, or a webcam>]"
+				 " [--output=<output file>]"
+				 " [--opacity=<the opacity of the sunglasses (0..1)>]"
+				 " [--reflectivity=<the reflectivity of the sunglasses (0..1)>]"
+				 " [--use_haar_eye_detector=<true or false>]"
+				 " [--eye_scale_factor=<float>]"
+				 " [--eye_min_neighbors=<integer>]"
+				 " [--face_scale_factor=<float>]"
+				 " [--face_min_neighbors=<integer>]" << std::endl;
+}
 
 int main(int argc, char* argv[])
 {
 	try
 	{
-		/*
+		
 		static const cv::String keys =
-			"{help h usage ? |      | Print the help message  }"
-			"{image          |<none>| The input image file  }"
-			"{confidence     |0.7   | Face detection confidence threshold }"
-			"{radius         |3     | Blur radius          }"
-			"{sigmac         |30    | Blur sigma in the color space }"
-			"{sigmas         |30    | Blur sigma in the coordinate space }"
-			"{heuristic      |2     | Skin detection heuristic: 1 - Mean color, 2 - Dominant color, 3 - Selective sampling }";
-		*/
+			"{help h usage ?        |                     | Print the help message  }"			
+			"{sunglasses            |./images/sunglass.png| The image of sunglasses to overlay with the input }"
+			"{reflection            |./images/lake.jpg    | The reflection image for sunglasses }"
+			"{input                 |cam:0                | The input image, video, or a webcam  }"
+			"{output                |                     | If not empty, specifies the output file }"
+			"{opacity               |0.5                  | The opacity of the sunglasses (0..1) }"
+			"{reflectivity          |0.4                  | The reflectivity of the sunglasses (0..1) }"
+			"{use_haar_eye_detector |false                | If set to true a Haar-based detector will be used instead of a proportional eye detector }"
+			"{eye_scale_factor      |1.1                  | The scale factor for the Haar-based eye detector }"
+			"{eye_min_neighbors     |3                    | How many neighbors each candidate should have to be retained by a Haar-based eye detector }"
+			"{face_scale_factor     |1.1                  | The scale factor for the Haar-based face detector }"
+			"{face_min_neighbors    |15                   | How many neighbors each candidate should have to be retained by a Haar-based face detector }";
+		
+		cv::CommandLineParser parser(argc, argv, keys);
+		parser.about("Glassify\n(c) Yaroslav Pugach");
+
+		if (parser.has("help"))
+		{
+			printUsage();
+			return 0;
+		}
+
+		std::string sunglassesFile = parser.get<std::string>("sunglasses");
+		std::string reflectionFile = parser.get<std::string>("reflection");
+		std::string input = parser.get<std::string>("input");
+		std::string output = parser.get<std::string>("output");
+		float opacity = parser.get<float>("opacity");
+		float reflectivity = parser.get<float>("reflectivity");
+		bool useHaarEyeDetector = parser.get<bool>("use_haar_eye_detector");
+		double eyeScaleFactor = parser.get<double>("eye_scale_factor");
+		int eyeMinNeighbors = parser.get<int>("eye_min_neighbors");
+		double faceScaleFactor = parser.get<double>("face_scale_factor");
+		int faceMinNeighbors = parser.get<int>("face_min_neighbors");
+
+		if (!parser.check())
+		{
+			parser.printErrors();
+			printUsage();
+			return -1;
+		}
 
 
 		auto webcamReader = MediaFactory::createReader("cAm");
